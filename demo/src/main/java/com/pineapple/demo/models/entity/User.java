0f01ae464ef8ba0cc688b07cb3e4c5f1441c2d84
Shel Valdés <creative@shelvaldes.com>
2021-11-09 22:19:30 -0600

    Modelado de User

---
+++ demo/src/main/java/com/pineapple/demo/models/entity/User.java
@@ -0,0 +1,83 @@
+package com.pineapple.demo.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="users")
+public class User implements Serializable{
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column (name= "id_user")
+	private Long idUser;
+	private String username;
+	private String password;
+	private String email;
+	@Column (name = "user_firstname")
+	private String firstName;
+	@Column (name = "user_lastname")
+	private String lastName;
+	//relation to id_role
+	
+	private static final long serialVersionUID = 1L;
+
+	public Long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+}
